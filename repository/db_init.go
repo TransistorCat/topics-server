fceae8395d3fd86b4bd088e9f0096379b356ad70
TransistorCat <1820737440@qq.com>
2023-07-29 16:43:26 +0800

    fix:解决包的循环引用

--- repository/db_init.go
+++ repository/db_init.go
@@ -1,12 +1,17 @@
 package repository
 
+import (
+	"github.com/TransistorCat/topics-server/repository/localfile"
+	"github.com/TransistorCat/topics-server/repository/mysql"
+)
+
 func Init(options Options) error {
 
 	switch options.DBType {
 	case 1:
-		InitLocalFile(&DefaultLocalFile)
+		localfile.Init()
 	case 2:
-		initMySQL()
+		mysql.Init()
 	}
 	return nil
 }
--- repository/mysql/init.go
+++ repository/mysql/init.go
@@ -7,7 +7,7 @@ import (
 
 var DB *gorm.DB
 
-func initMySQL() error {
+func Init() error {
 	dataSourceName := "root:root@tcp(127.0.0.1:3306)/TopicServerDB?charset=utf8&parseTime=True"
 	var err error
 	if DB, err = gorm.Open(mysql.Open(dataSourceName), nil); err != nil {
